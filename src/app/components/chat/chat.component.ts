4dc2d03f166b0406f9e5ac101030fe846ec380f5
Eric Martínez <emartinez@grupodot.com>
2019-02-11 10:20:35 -0500

    Actualizate Repo.

---
+++ src/app/components/chat/chat.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+import { ChatService } from "../../providers/chat.service";
+
+@Component({
+  selector: 'app-chat',
+  templateUrl: './chat.component.html',
+  styles: []
+})
+export class ChatComponent implements OnInit{
+
+  message: string = "";
+  elemento: any;
+
+  constructor( public _cs: ChatService ) {
+    this._cs.cargarMensajes()
+        .subscribe( () => {
+          setTimeout(() => {
+              this.elemento.scrollTop = this.elemento.scrollHeight;
+          }, 20)
+        });
+  }
+
+  ngOnInit(): void {
+    this.elemento =  document.getElementById('app-mensajes');
+  }
+
+    send_message(){
+    console.log( this.message );
+
+    if (this.message.length === 0){
+      return;
+    }
+
+    this._cs.agregarMensaje( this.message )
+        .then(() => this.message = "" )
+        .catch((err) => console.error( 'Error al enviar', err ));
+
+  }
+
+}
